3332ed93847f0b47e646a6ea8818c84736049d48
Mike Bertram <contact@mibexx.de>
2018-07-17 12:48:29 +0200

    * Code fixes

--- src/Xervice/Routing/Business/Route/RouteCollection.php
+++ src/Xervice/Routing/Business/Route/RouteCollection.php
@@ -5,7 +5,9 @@ namespace Xervice\Routing\Business\Route;
 
 
 use DataProvider\RouteCollectionDataProvider;
+use DataProvider\RouteDataProvider;
 use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\Route as SymfonyRoute;
 use Symfony\Component\Routing\RouteCollection as SymfonyRouteCollection;
 
 class RouteCollection implements RouteCollectionInterface
@@ -41,16 +43,96 @@ class RouteCollection implements RouteCollectionInterface
         foreach ($dataProvider->getRoutes() as $route) {
             $this->routeCollection->add(
                 $route->getName(),
-                new Route(
-                    $route->hasPath() ? $route->getPath() : '/',
-                    $route->hasDefaults() ? $route->getDefaults() : [],
-                    $route->hasRequirements() ? $route->getRequirements() : [],
-                    $route->hasOptions() ? $route->getOptions() : [],
-                    $route->hasHost() ? $route->getHost() : '',
-                    $route->hasSchemes() ? $route->getSchemes() : [],
-                    $route->hasMethods() ? $route->getMethods() : []
-                )
+                $this->getSymfonyRouteFromDataProvider($route)
             );
         }
+    }
+
+    /**
+     * @param \DataProvider\RouteDataProvider $route
+     *
+     * @return \Symfony\Component\Routing\Route
+     */
+    private function getSymfonyRouteFromDataProvider(RouteDataProvider $route): SymfonyRoute
+    {
+        return new Route(
+            $this->getRoutePath($route),
+            $this->getRouteDefaults($route),
+            $this->getRouteRequirements($route),
+            $this->getRouteOptions($route),
+            $this->getRouteHost($route),
+            $this->getRouteSchemas($route),
+            $this->getRouteMethods($route)
+        );
+    }
+
+    /**
+     * @param \DataProvider\RouteDataProvider $route
+     *
+     * @return string
+     */
+    private function getRoutePath(RouteDataProvider $route): string
+    {
+        return $route->hasPath() ? $route->getPath() : '/';
+    }
+
+    /**
+     * @param \DataProvider\RouteDataProvider $route
+     *
+     * @return array
+     */
+    private function getRouteDefaults(RouteDataProvider $route): array
+    {
+        return $route->hasDefaults() ? $route->getDefaults() : [];
+    }
+
+    /**
+     * @param \DataProvider\RouteDataProvider $route
+     *
+     * @return array
+     */
+    private function getRouteRequirements(RouteDataProvider $route): array
+    {
+        return $route->hasRequirements() ? $route->getRequirements() : [];
+    }
+
+    /**
+     * @param \DataProvider\RouteDataProvider $route
+     *
+     * @return array
+     */
+    private function getRouteOptions(RouteDataProvider $route): array
+    {
+        return $route->hasOptions() ? $route->getOptions() : [];
+    }
+
+    /**
+     * @param \DataProvider\RouteDataProvider $route
+     *
+     * @return string
+     */
+    private function getRouteHost(RouteDataProvider $route): string
+    {
+        return $route->hasHost() ? $route->getHost() : '';
+    }
+
+    /**
+     * @param \DataProvider\RouteDataProvider $route
+     *
+     * @return array
+     */
+    private function getRouteSchemas(RouteDataProvider $route): array
+    {
+        return $route->hasSchemes() ? $route->getSchemes() : [];
+    }
+
+    /**
+     * @param \DataProvider\RouteDataProvider $route
+     *
+     * @return array
+     */
+    private function getRouteMethods(RouteDataProvider $route): array
+    {
+        return $route->hasMethods() ? $route->getMethods() : [];
     }
 }
